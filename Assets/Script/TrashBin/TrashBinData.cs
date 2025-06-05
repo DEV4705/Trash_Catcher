62dc4e0e0afae28a6bc6a980adcd09093ad80d68
Cel <marcell.sutiono@gmail.com>
2025-06-05 09:58:48 +0700

    feat : Trash Randomizer

--- Assets/Script/TrashBin/TrashBinData.cs
+++ Assets/Script/TrashBin/TrashBinData.cs
@@ -3,10 +3,18 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "TrashBinData", menuName = "Scriptable Objects/TrashBinData")]
 public class TrashBinData : ScriptableObject
 {
-    private float speed = 5f;
+    //TrashBin data
+    private float speed = 7f;
     private int score = 0;
 
+    //Types
+    private bool foodWaste = true;
+    private bool paperWaste = false;
+    private bool plasticWaste = false;
+    private bool metalWaste = false;
+
     public static TrashBinData Instance;
+
 
     public static TrashBinData getInstance()
     {
@@ -19,6 +27,10 @@ public class TrashBinData : ScriptableObject
 
     public float Speed { get { return speed; } set { speed = value; }}
     public int Score { get { return score; } set { score = value; } }
+    public bool FoodWaste { get { return foodWaste; } set { foodWaste = value; } }
+    public bool PaperWaste { get { return paperWaste; } set { paperWaste = value; } }
+    public bool PlasticWaste { get { return plasticWaste; } set { plasticWaste = value; } }
+    public bool MetalWaste { get { return metalWaste; } set { metalWaste = value; } }
 
     //notes ini cara pakenya nanti di kelas lain tinggal bikin [TrashBinData tb = TrashBinData.getInstance();]
 }
--- Assets/Script/TrashBin/TrashBinMovement.cs
+++ Assets/Script/TrashBin/TrashBinMovement.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel;
 using UnityEngine;
 
 public class TrashBinMovement : MonoBehaviour
 {
     //Trash bin
+    [SerializeField] private GameObject trashBin;
+    private SpriteRenderer sr;
+
     private Rigidbody2D rb;
     private TrashBinData data;
+    private ChangeType changer;
 
     //Movement
     private float dir;
@@ -12,13 +17,16 @@ public class TrashBinMovement : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
         data = TrashBinData.getInstance();
+        changer = new ChangeType();
     }
 
     // Update is called once per frame
     private void Update()
     {
         move();
+        changeType();
     }
 
     private void move()
@@ -26,4 +34,30 @@ public class TrashBinMovement : MonoBehaviour
         dir = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2(dir * data.Speed, rb.linearVelocity.y);
     }
+
+    private void changeType()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            changer.changeToFoodWaste();
+            sr.color = Color.green;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            changer.changeToPaperWaste();
+            sr.color = Color.white;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            changer.changeToPlasticWaste();
+            sr.color = Color.blue;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            changer.changeToMetalWaste();
+            sr.color = Color.gray;
+            //notes pergantian warna diganti sprite nantinya
+        }
+    }
+
 }
